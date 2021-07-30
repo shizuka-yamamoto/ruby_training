20894a8621399e3cbf2c27b9743977179cdab24a
shizuka <shiiishim920@gmail.com>
2021-07-30 19:51:16 +0900

    ハッシュの問題

--- sort.rb
+++ sort.rb
@@ -1,17 +1,39 @@
-p ["コーヒー",  "カフェラテ"].size
-p [1, 2, 3, 4, 5].sum
-p ["モカ", "カフェラテ", "モカ"].uniq
-p ["モカ", "カフェラテ", "モカ"].clear
-p ["コーヒー",  "カフェラテ", "カプチーノ"].sample
-p ["大吉", "中吉", "末吉", "凶"].sample
-p [100, 50, 300].sort
-p [100, 50, 300].sort.reverse
-p "cba".reverse
-p ["100", "50", "300"].join(",")
-p "100,50,300".split(",")
-p [1, 2, 3].map {|num| num * 3}
-p ["abc", "xyz"].map {|x|x.reverse}
-array = ["aya", "achi", "Tama"]. map do |name|
-    name.downcase
+menu = {coffee: 300, caffe_latte: 400}
+p menu[:caffe_latte]
+
+menu = {"モカ" => "チョコレートシロップとミルク入り",  "カフェラテ" => "ミルク入り"}
+puts menu["モカ"]
+
+menu = {coffee: 300, caffe_latte: 400}
+menu[:tea] = 300
+p menu
+
+menu = {coffee: 300, caffe_latte: 400}
+menu.delete(:coffee)
+p menu
+
+menu = {coffee: 300, caffe_latte: 400}
+unless menu[:tea]
+  puts "紅茶はありませんか"
 end
-p array.sort
+
+menu = {coffee: 300, caffe_latte: 400}
+if menu[:caffe_latte] <= 500
+  puts "カフェラテください"
+end
+
+hash = {}
+hash.default = 0
+array = "caffelatte".chars
+array.each do |str|
+  hash[str] += 1
+end
+p hash
+
+menu = {"コーヒー" => 300, "カフェラテ" => 400}
+menu.each do |key, value|
+  p "#{key} - #{value}" if value >= 350
+end
+
+menu = {"コーヒー" => 300, "カフェラテ" => 400}
+p menu.keys
