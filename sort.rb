a4e87f826013ccd887540cd6fd5c9a683c06fc49
shizuka <shiiishim920@gmail.com>
2021-07-31 18:27:05 +0900

    ソートアルゴリズム

--- sort.rb
+++ sort.rb
@@ -1,40 +1,16 @@
-def order
-  puts "カフェラテください"
-end
-order
-
-def area(x)
-  x * x
-end
-
-puts area(3)
-
-def dice
-  [1, 2, 3, 4, 5, 6].sample  
-end
-
-p dice
-
-def order(item)
-  puts "#{item}ください"
+def bubble_sort(ary)
+  length = ary.length
+  (1..length-1).each do |i| #ループA：1〜「要素数-1」まで
+    (1..(length-i)).each  do |jx| #ループB：1〜「要素数-ループAの現在の値」まで
+      jy = jx - 1 
+      if ary[jy] > ary[jx] #「ループBの現在の値」番目の要素 > 「ループBの現在の値+1」番目の要素
+        tmp = ary[jy]      #「ループBの現在の値」番目の要素 と「ループBの現在の値+1」番目の要素を入れ替える
+        ary[jy] = ary[jx]
+        ary[jx] = tmp
+      end 
+    end
+  end
+  return ary
 end
 
-order("カフェラテ")
-order("モカ")
-
-def dice
-  num = [1, 2, 3, 4, 5, 6].sample
-  return num unless num == 1
-  puts "もう一回"
-  num = [1, 2, 3, 4, 5, 6].sample
-end
-p dice
-
-def price(item:, size: "ショート")
-  items = {"コーヒー" => 300, "カフェラテ" => 400}
-  sizes = {"ショート" => 0, "トール" => 50, "ベンティ" => 100}
-  items[item] + sizes[size]
-end
-
-p price(item: "コーヒー")
-p price(item: "カフェラテ", size: "ベンティ")
+p bubble_sort([10, 8, 3, 5, 2, 4, 11, 18, 20, 33])
