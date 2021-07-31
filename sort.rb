b4f14dcd26a0065072c9dc0c340a5fed6f35c571
shizuka <shiiishim920@gmail.com>
2021-07-31 18:37:33 +0900

    ソートアルゴリズム

--- sort.rb
+++ sort.rb
@@ -2,7 +2,7 @@ def bubble_sort(ary)
   length = ary.length
   (1..length-1).each do |i| #ループA：1〜「要素数-1」まで
     (1..(length-i)).each  do |jx| #ループB：1〜「要素数-ループAの現在の値」まで
-      jy = jx - 1 
+      jy = jx - 1
       if ary[jy] > ary[jx] #「ループBの現在の値」番目の要素 > 「ループBの現在の値+1」番目の要素
         tmp = ary[jy]      #「ループBの現在の値」番目の要素 と「ループBの現在の値+1」番目の要素を入れ替える
         ary[jy] = ary[jx]
