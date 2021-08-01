b8e758059be6c7bf2abf9ccb5239d26ac838470b
shizuka <shiiishim920@gmail.com>
2021-08-01 11:09:21 +0900

    半角スペース・returnの取り消し

--- sort.rb
+++ sort.rb
@@ -1,7 +1,7 @@
 def bubble_sort(ary)
   length = ary.length
-  (1..length-1).each do |i| #ループA：1〜「要素数-1」まで
-    (1..(length-i)).each  do |jx| #ループB：1〜「要素数-ループAの現在の値」まで
+  (1..length - 1).each do |i| #ループA：1〜「要素数-1」まで
+    (1..(length - i)).each  do |jx| #ループB：1〜「要素数-ループAの現在の値」まで
       jy = jx - 1
       if ary[jy] > ary[jx] #「ループBの現在の値」番目の要素 > 「ループBの現在の値+1」番目の要素
         tmp = ary[jy]      #「ループBの現在の値」番目の要素 と「ループBの現在の値+1」番目の要素を入れ替える
@@ -10,7 +10,6 @@ def bubble_sort(ary)
       end 
     end
   end
-  return ary
 end
 
 p bubble_sort([10, 8, 3, 5, 2, 4, 11, 18, 20, 33])
